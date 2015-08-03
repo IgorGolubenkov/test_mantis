4302a782fc5178b86df1ee67236366f15b3c25b4
Igor Golubenkov <i.golubenkov@mail.ru>
2015-08-03 18:08:54 +0300

    Реализован тест на проверку логина.

--- conftest.py
+++ conftest.py
@@ -2,11 +2,10 @@
 
 import pytest
 from fixture.application import Application
-from fixture.db import DbFixture
+#from fixture.db import DbFixture
 import json
 import os.path
 import importlib
-import jsonpickle
 
 
 fixture = None
@@ -28,18 +27,18 @@ def app(request):
     web_config = load_config(request.config.getoption("--target"))["web"]
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=web_config['baseUrl'])
-    fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
+    #fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
     return fixture
 
 
-@pytest.fixture(scope="session")
-def db(request):
-    db_config = load_config(request.config.getoption("--target"))["db"]
-    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
-    def fin():
-        dbfixture.destroy()
-    request.addfinalizer(fin)
-    return dbfixture
+#@pytest.fixture(scope="session")
+#def db(request):
+    #db_config = load_config(request.config.getoption("--target"))["db"]
+    #dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
+    #def fin():
+        #dbfixture.destroy()
+    #request.addfinalizer(fin)
+    #return dbfixture
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -62,20 +61,20 @@ def pytest_addoption(parser):
     parser.addoption("--check_ui", action="store_true")
 
 
-def pytest_generate_tests(metafunc):
-    for fixture in metafunc.fixturenames:
-        if fixture.startswith("data_"):
-            testdata = load_from_module(fixture[5:])
-            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
-        elif fixture.startswith("json_"):
-            testdata = load_from_json(fixture[5:])
-            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+#def pytest_generate_tests(metafunc):
+    #for fixture in metafunc.fixturenames:
+        #if fixture.startswith("data_"):
+            #testdata = load_from_module(fixture[5:])
+            #metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        #elif fixture.startswith("json_"):
+            #testdata = load_from_json(fixture[5:])
+            #metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
 
 
 def load_from_module(module):
     return importlib.import_module("data.%s" % module).testdata
 
 
-def load_from_json(file):
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
-        return jsonpickle.decode(f.read())
+#def load_from_json(file):
+    #with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        #return jsonpickle.decode(f.read())
